6df83b7c717eb767655c8fe0f62f3e6744813f88
Your Name <alessanrospedroso@outlook.com>
2025-03-10 09:00:41 -0300

    primeiro commit

--- App.js
+++ App.js
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
 
 export default function App() {
+  const [feed, setFeed] = useState([
+    {
+      id: "1",
+      nome: "Alessandro",
+      idade: 50,
+      email: "alessandropedrosoti@gmail.com",
+    },
+    {
+      id: "2",
+      nome: "Eduardo",
+      idade: 10,
+      email: "eduardo@gmail.com",
+    },
+    {
+      id: "3",
+      nome: "Taís",
+      idade: 10,
+      email: "Taís@gmail.com",
+    },
+    {
+      id: "4",
+      nome: "Lúcia",
+      idade: 50,
+      email: "lucia@gmail.com",
+    },
+    {
+      id: "5",
+      nome: "José",
+      idade: 50,
+      email: "jose@gmail.com",
+    },
+  ]);
   return (
+    //para usar uma key no FlatList usar o keyExtractor={(item)=> item._id}
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList
+        showsVerticalScrollIndicator={false}
+        data={feed}
+        renderItem={({ item }) => <Pessoa data={item} />}
+      />
     </View>
   );
 }
@@ -13,8 +50,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  areaPessoa: {
+    backgroundColor: "#121212",
+    height: 200,
+    marginBottom: 15,
+  },
+  textoPessoa: {
+    color: "#FFF",
+    fontSize: 20,
   },
 });
+
+function Pessoa(props) {
+  return (
+    <View style={styles.areaPessoa}>
+      <Text style={styles.textoPessoa}>{props.data.nome}</Text>
+      <Text style={styles.textoPessoa}>{props.data.idade}</Text>
+      <Text style={styles.textoPessoa}>{props.data.email}</Text>
+    </View>
+  );
+}
